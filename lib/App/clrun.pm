fd9d5a7d5fb0f20137d31c38294c02afd1bca4eb
fukamachi <e.arrows@gmail.com>
2012-04-14 10:48:17 +0900

    draft

--- lib/App/clrun.pm
+++ lib/App/clrun.pm
@@ -1,111 +1,27 @@
 package App::clrun;
 
-use 5.006;
 use strict;
 use warnings;
 
-=head1 NAME
+use Getopt::Long;
 
-App::clrun - The great new App::clrun!
+sub new {
+    my ($class) = @_;
 
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
-
-
-=head1 SYNOPSIS
-
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use App::clrun;
-
-    my $foo = App::clrun->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
-
-=cut
-
-sub function1 {
+    return bless { argv => [] }, $class;
 }
 
-=head2 function2
+sub parse_options {
+    my ( $self, @argv ) = @_;
 
-=cut
+    local @ARGV = @{ $self->{argv} };
+    push @ARGV, @_;
 
-sub function2 {
+    $self->{argv} = \@ARGV;
 }
 
-=head1 AUTHOR
+sub doit {
+    my ($self) = @_;
+}
 
-Eitarow Fukamachi, C<< <e.arrows at gmail.com> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-app-clrun at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-clrun>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc App::clrun
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=App-clrun>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/App-clrun>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/App-clrun>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/App-clrun/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2012 Eitarow Fukamachi.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See http://dev.perl.org/licenses/ for more information.
-
-
-=cut
-
-1; # End of App::clrun
+1;
